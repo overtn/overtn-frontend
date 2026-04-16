0983c29ff9be29c79d35393bc3d059eb9d6ad5dd
Савелий Морозенко <morozque@MacBook-Pro-Savelij.local>
2026-04-16 17:11:58 +0300

    Replace CDEK widget prep with Yandex PVZ map

--- js/config.js
+++ js/config.js
@@ -1,28 +1,6 @@
 window.APP_CONFIG = {
   API_BASE_URL: "https://overtn-backend.onrender.com",
-  CDEK_WIDGET: {
-    scriptUrl: "https://cdn.jsdelivr.net/gh/cdek-it/widget@3/dist/cdek-widget.umd.js",
-    apiKey: "",
-    servicePath: "",
-    rootId: "cdek-widget-slot",
-    from: {
-      country_code: "RU",
-      city: "Москва",
-    },
-    hideDeliveryOptions: {
-      door: true,
-      pickup: true,
-    },
-    hideFilters: {
-      type: true,
-    },
-    forceFilters: {
-      type: "PVZ",
-    },
-    lang: "rus",
-    currency: "RUB",
-    fixBounds: "country",
-  },
+  YANDEX_MAPS_API_KEY: "",
 };
 
 window.apiRequest = async (path, options = {}) => {
